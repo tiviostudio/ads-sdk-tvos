1f1d58e937923c08994bf9bb0e98a6483b9fe99a
Jan (Honza) Jiráň <jan.jiran@tivio.studio>
2024-04-09 14:13:50 +0200

    feat(tivio): init

--- Package.swift
+++ Package.swift
@@ -1,23 +1,33 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
+
 let package = Package(
-    name: "sdk-tvos",
+    name: "Tivio",
+    defaultLocalization: "en",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "sdk-tvos",
-            targets: ["sdk-tvos"]),
+            name: "Tivio",
+            targets: ["Tivio"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "sdk-tvos"),
-        .testTarget(
-            name: "sdk-tvosTests",
-            dependencies: ["sdk-tvos"]),
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+//        .target(
+//            name: "sdk-tvos"
+//        ),
+//        .binaryTarget(
+//            name: "SomeRemoteBinaryPackage",
+//            url: "https://url/to/some/remote/xcframework.zip",
+//            checksum: "The checksum of the ZIP archive that contains the XCFramework."
+//        ),
+        .binaryTarget(
+            name: "Tivio",
+            path: "Tivio.xcframework"
+        )
     ]
 )
